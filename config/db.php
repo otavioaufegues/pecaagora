d249b3b276a9b93dc864a5faab6a7b185b9c1050
Otavio Augusto <otavioaufegues@gmail.com>
2022-05-19 00:17:12 -0300

    conexao no banco de dados

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'pgsql:host=localhost;port=5432;dbname=postgres',
+    'username' => 'postgres',
+    'password' => '123456',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
