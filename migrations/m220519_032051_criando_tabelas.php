fe320d589ecd5abceb0efbe916ff232d73861253
Otavio Augusto <otavioaufegues@gmail.com>
2022-05-19 00:29:40 -0300

    migration

---
+++ migrations/m220519_032051_criando_tabelas.php
@@ -0,0 +1,42 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m220519_032051_criando_tabelas
+ */
+class m220519_032051_criando_tabelas extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        echo "m220519_032051_criando_tabelas cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m220519_032051_criando_tabelas cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
