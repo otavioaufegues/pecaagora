54c95f525564d54f8dbb294c03f276af4847a120
Otavio Augusto <otavioaufegues@gmail.com>
2022-05-20 00:17:25 -0300

    add migrations

--- migrations/m220519_032051_criando_tabelas.php
+++ migrations/m220519_032051_criando_tabelas.php
@@ -1,6 +1,7 @@
 <?php
 
 use yii\db\Migration;
+use yii\db\Schema;
 
 /**
  * Class m220519_032051_criando_tabelas
@@ -12,7 +13,27 @@ class m220519_032051_criando_tabelas extends Migration
      */
     public function safeUp()
     {
+        $this->createTable('funcionario', [
+            'id' => Schema::TYPE_PK,
+            'nome' => Schema::TYPE_STRING . ' NOT NULL',
+            'cpf' => Schema::TYPE_STRING,
+            'logradouro' => Schema::TYPE_STRING,
+            'cep' => Schema::TYPE_STRING,
+            'cidade' => Schema::TYPE_STRING,
+            'estado' => Schema::TYPE_STRING,
+            'numero' => Schema::TYPE_STRING,
+            'complemento' => Schema::TYPE_STRING,
+            'cargo_id' => Schema::TYPE_INTEGER . ' NOT NULL DEFAULT 1',
+        ]);
 
+        $this->createTable('cargo', [
+            'id' => Schema::TYPE_PK,
+            'nome' => Schema::TYPE_STRING . ' NOT NULL',
+        ]);
+
+        $this->batchInsert('cargo', [
+            'nome'
+        ], [['Gerente'], ['Vendedor'], ['Estagiário']]);
     }
 
     /**
@@ -21,8 +42,8 @@ class m220519_032051_criando_tabelas extends Migration
     public function safeDown()
     {
         echo "m220519_032051_criando_tabelas cannot be reverted.\n";
-
-        return false;
+        $this->delete('noticias', ['id' => 1]);
+        $this->dropTable('noticias');
     }
 
     /*
